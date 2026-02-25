3024abbac4958f042eddc5d11c63142ffa29f7d7
ketankakde <104485918+ketankakde@users.noreply.github.com>
2026-02-25 15:53:22 +0530

    Add AWS EKS infrastructure configuration

---
+++ main.tf
@@ -0,0 +1,125 @@
+provider "aws" {
+   region = "us-east-1"
+}  
+    
+terraform {
+   backend = "s3" {
+      bucket = "3-tier-bket"
+      key = "terraform.tfstate"
+      region = "us-east-1"
+      }
+}      
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_cluster_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+data "aws_vpc" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = ["default-vpc"]
+  }
+}
+
+data "aws_subnets" "existing" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "my-eks"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+   
+  vpc_config {
+    subnet_ids = data.aws_subnets.existing.ids
+  }
+}
+    depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "vpc-cni"
+  addon_version = "v1.20.4-eksbuild.2"
+  depends_on = [aws_eks_cluster.eks]
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "kube-proxy"
+  addon_version = "v1.34.0-eksbuild.2"
+  depends_on = [aws_eks_cluster.eks]
+}
+
+
+
+resource "aws_iam_role" "node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_eks_node_group" "nodes" {
+  cluster_name  = aws_eks_cluster.eks.name
+  node_role_arn = aws_iam_role.node_role.arn
+
+  subnet_ids = data.aws_subnets.existing.ids
+
+  instance_types = ["m7i-flex.large"]
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+  depends_on = [ aws_iam_role_policy_attachment.worker_node_policy,
+  aws_iam_role_policy_attachment.cni_policy,
+  aws_iam_role_policy_attachment.ecr_policy ]
+}
